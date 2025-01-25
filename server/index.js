115ffb47785c525cf60c3e19b5cda810e06a3bb5
Nitish Kumar Mohanty <nitishmohanty1@gmail.com>
2025-01-25 23:13:36 +0530

    feat: add note model and endpoint for adding notes with validation

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 mongoose.connect(config.connectionString);
 
 const User = require("./models/user.models");
+const Note = require("./models/note.model")
 
 const express = require("express");
 const cors = require("cors");
@@ -69,6 +70,7 @@ app.post("/create-account", async (req, res) => {
   });
 });
 
+//Login
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
@@ -107,6 +109,47 @@ app.post("/login", async (req, res) => {
         message:"Invalid credentials",
     });
   }
+});
+
+//Add-Notes
+app.post("/add-note",authenticationToken, async(req,res)=>{
+  const {title,content,tags}=req.body;
+  const {user} = req.user;
+
+  if (!title) {
+    return res.status(400).json({
+      message: "Title is required",
+    });
+  }
+
+  if (!content) {
+    return res.status(400).json({
+      message: "Content is required",
+    });
+  }
+
+  try{
+    const note = new Note({
+      title,
+      content,
+      tags: tags || [],
+      userId:user._id,
+    });
+
+    await note.save();
+
+    return res.json({
+      error:false,
+      note,
+      message:"Note added Successfully"
+    });
+  } catch(error){
+    return res.status(500).json({
+      error:true,
+      message:"Internal server Error"
+    });
+  }
+
 });
 
 app.listen(8000);
